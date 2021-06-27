c12eecb83c6fc36d17d242ac7c731d6b1792ffb8
Decipulo <85293733+Decipulo@users.noreply.github.com>
2021-06-27 12:50:24 +0800

    Update admin.py

--- FORPWD/admin.py
+++ FORPWD/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Applicant,Account,Discussion,Comment,ProductItem,login
+from .models import Applicant,Account,Discussion,Comment,ProductItem,login,Donation
 # Register your models here.Address,
 
 admin.site.register(Applicant)
@@ -8,3 +8,4 @@ admin.site.register(Discussion)
 admin.site.register(Comment)
 admin.site.register(ProductItem)
 admin.site.register(login)
+admin.site.register(Donation)
